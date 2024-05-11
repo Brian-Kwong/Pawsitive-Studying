9a2d778605e95e5344a70a615731bfa66a052752
Brian Kwong <bkwong01@calpoly.edu>
2024-05-10 22:30:18 -0700

    Fixed typo

--- Application/Pawsitive-Studying/Styles/comp_styles.jsx
+++ Application/Pawsitive-Studying/Styles/comp_styles.jsx
@@ -66,7 +66,6 @@ export const styles = StyleSheet.create({
         margin: '5%, 5%, 5%, 5%',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     },
-    },
     TabsButton: {
         fontSize: 20,
         borderRadius: 10,
