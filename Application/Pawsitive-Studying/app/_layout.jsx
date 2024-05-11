f6f46d8186e1a4ab449c18496f07259ab3a599e1
Brian-Kwong <Brian-Kwong@users.noreply.github.com>
2024-05-11 01:25:49 +0000

    ESLint updated files

--- Application/Pawsitive-Studying/app/_layout.jsx
+++ Application/Pawsitive-Studying/app/_layout.jsx
@@ -1,10 +1,10 @@
-import { Stack } from 'expo-router/stack'
-import 'react-native-reanimated'
+import { Stack } from "expo-router/stack";
+import "react-native-reanimated";
 
 export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="(Tabs)" options={{ headerShown: false }} />
         </Stack>
-    )
+    );
 }
