81b4bc34ac51267db40bfffe3578157dd9e73007
Warrior-li <lishuxuan.0304@gmail.com>
2024-05-15 02:31:09 +0800

    add backend project

---
+++ Application/backend-express/backend.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PWD = process.env.MONGO_PWD;
+const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
+
+const uri = "mongodb://127.0.0.1:27017/csc";
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(
+        `app listening at http://localhost:${port}`
+    );
+});
--- Application/backend-express/schema.js
+++ Application/backend-express/schema.js
@@ -1,109 +1,101 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        pronouns: {
-            type: String,
-            required: false,
-            trim: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        memberSince: {
-            type: String,
-            required: true,
-            default: Date().now,
-        },
-        profileImage: {
-            data: Buffer,
-            required: false,
-        },
-        tasks: [
-            {
-                name: {
-                    type: String,
-                    required: true,
-                    trim: true,
-                },
-                course: {
-                    type: String,
-                    required: false,
-                    trim: true,
-                    default: 'None',
-                },
-                description: {
-                    type: String,
-                    required: false,
-                    trim: true,
-                    default: 'None',
-                },
-                time: {
-                    type: Number,
-                    required: true,
-                    default: 0,
-                },
-                points: {
-                    type: Number,
-                    required: true,
-                    default: 0,
-                },
-            },
-        ],
-        characters: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Characters',
-            },
-        ],
-    },
-    { collection: 'Users' }
-)
+const Schema = mongoose.Schema;
 
-const charactersSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        description: {
-            type: String,
-            required: false,
-            trim: true,
-            default: 'None',
-        },
-        pointsRequired: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        image: {
-            data: Buffer,
-            required: false,
-        },
-    },
-    { collection: 'Characters' }
-)
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	pronouns: {
+		type: String,
+		required: false,
+		trim: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	memberSince: {
+		type: Date,
+		required: true,
+		default: Date.now, // 使用 Date.now 作为默认值生成函数
+	},
+	profileImage: {
+		data: Buffer,
+		required: false
+	},
+	tasks: [{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		course: {
+			type: String,
+			required: false,
+			trim: true,
+			default: "None"
+		},
+		description: { // 在此加入冒号
+			type: String,
+			required: false,
+			trim: true,
+			default: "None"
+		},
+		time: {
+			type: Number,
+			required: true,
+			default: 0
+		},
+		points: {
+			type: Number,
+			required: true,
+			default: 0
+		}
+	}],
+	characters: [{
+		type: Schema.Types.ObjectId, // 修正数据类型，添加逗号
+		ref: "Characters"
+	}]
+}, { collection: "Users" }); // 确保包含结束的大括号和小括号
 
-const User = mongoose.model('User', userSchema)
-const Character = mongoose.model('Characters', charactersSchema)
+const charactersSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	description: {
+		type: String,
+		required: false,
+		trim: true,
+		default: "None"
+	},
+	pointsRequired: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	image: {
+		data: Buffer,
+		required: false
+	}
+}, { collection: "Characters" })
 
-export default { User, Character }
+const User = mongoose.model("User", userSchema);
+const Character = mongoose.model("Characters", charactersSchema);
+
+export default { User, Character };
