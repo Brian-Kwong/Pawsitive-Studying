1c31cadea786e20af0911af932487b9f2c135fdb
jzaval17 <jzaval17@calpoly.edu>
2024-05-14 23:54:19 -0700

    correct branch?

--- Application/backend-express/backend.js
+++ Application/backend-express/backend.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-import { registerUser } from "./auth.js";
+import { registerUser, getUser } from "./auth.js";
+
 dotenv.config();
 
 const app = express();
@@ -18,12 +19,15 @@ const mongoCluster = process.env.MONGO_CLUSTER;
 let mongoURI;
 
 if (mongoUser && mongoPwd) {
-    mongoURI = `mongodb://${mongoUser}:${mongoPwd}@localhost:27017/${mongoCluster}?authSource=admin`;
+    mongoURI = `mongodb+srv://${mongoUser}:${mongoPwd}@${mongoCluster}.mongodb.net/myDatabase?retryWrites=true&w=majority`;
 } else {
     mongoURI = `mongodb://localhost:27017/${mongoCluster}`;
 }
 
-mongoose.connect(mongoURI).then(() => {
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
@@ -31,8 +35,8 @@ mongoose.connect(mongoURI).then(() => {
 
 app.post("/signup", registerUser);
 
+app.get("/user/:username", getUser);
+
 app.listen(port, () => {
-    console.log(
-        `app listening at http://localhost:${port}`
-    );
+    console.log(`app listening at http://localhost:${port}`);
 });
