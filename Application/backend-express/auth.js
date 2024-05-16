5940f13bcb1863239fd3f75775a0d796c78f786a
Brian Kwong <bkwong01@calpoly.edu>
2024-05-15 21:31:29 -0700

    Tested User Login and Signup

--- Application/backend-express/auth.js
+++ Application/backend-express/auth.js
@@ -1,30 +1,49 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import { User, Character } from './schema.js'
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
+const creds = []
 
-const creds = [];
+export async function conflictUser(username) {
+    User.findOne({ username }).then((user) => {
+        console.log(user)
+        if (user === null) {
+            console.log('User does not exist')
+            return false
+        }
+        return true
+    })
+}
 
 export async function registerUser(req, res) {
-    const { name, pronouns, username, email, password, profileImage, tasks, characters } = req.body;
+    const {
+        name,
+        pronouns,
+        username,
+        email,
+        password,
+        profileImage,
+        tasks,
+        characters,
+    } = req.body
     console.log(req.body)
 
     if (!name || !username || !email || !password) {
-        return res.status(400).send("Bad request: Invalid input data.");
+        return res.status(400).send('Bad request: Invalid input data.')
     }
 
     try {
         // 检查用户名是否已存在
-        const existingUser = await User.findOne({ username });
+        const existingUser = await User.findOne({ username })
         if (existingUser) {
-            return res.status(409).send("Username already taken");
+            return res.status(409).send('Username already taken')
         }
 
         // 哈希密码
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         // 创建新用户
         const newUser = new User({
@@ -36,18 +55,17 @@ export async function registerUser(req, res) {
             memberSince: new Date(),
             profileImage,
             tasks,
-            characters
-        });
+            characters,
+        })
 
         // 保存用户到数据库
-        await newUser.save();
+        await newUser.save()
 
         // 生成JWT令牌
-        const token = await generateAccessToken(username);
-        res.status(201).send({ token });
-
+        const token = await generateAccessToken(username)
+        res.status(201).send({ token })
     } catch (error) {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error')
     }
 }
 
@@ -56,65 +74,64 @@ function generateAccessToken(username) {
         jwt.sign(
             { username: username },
             process.env.TOKEN_SECRET,
-            { expiresIn: "1d" },
+            { expiresIn: '1d' },
             (error, token) => {
                 if (error) {
-                    reject(error);
+                    reject(error)
                 } else {
-                    resolve(token);
+                    resolve(token)
                 }
             }
-        );
-    });
+        )
+    })
 }
 
 export function authenticateUser(req, res, next) {
-    const authHeader = req.headers["authorization"];
+    const authHeader = req.headers['authorization']
     // 获取认证头的第二部分（令牌）
-    const token = authHeader && authHeader.split(" ")[1];
+    const token = authHeader && authHeader.split(' ')[1]
     if (!token) {
-        console.log("No token received");
-        res.status(401).end();
+        console.log('No token received')
+        res.status(401).end()
     } else {
-        jwt.verify(
-            token,
-            process.env.TOKEN_SECRET,
-            (error, decoded) => {
-                if (decoded) {
-                    next();
-                } else {
-                    console.log("JWT error:", error);
-                    res.status(401).end();
-                }
+        jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+            if (decoded) {
+                next()
+            } else {
+                console.log('JWT error:', error)
+                res.status(401).end()
             }
-        );
+        })
     }
 }
 
 export async function loginUser(req, res) {
-    const { username, password } = req.body; // 从表单获取
+    const { username, password } = req.body // 从表单获取
     if (!username || !password) {
-        return res.status(400).send("Bad request: Invalid input data.");
+        return res.status(400).send('Bad request: Invalid input data.')
     }
 
     try {
         // 查找用户
-        const retrievedUser = await User.findOne({ username });
+        const retrievedUser = await User.findOne({ username })
         if (!retrievedUser) {
-            return res.status(401).send("Unauthorized: Invalid username or password");
+            return res
+                .status(401)
+                .send('Unauthorized: Invalid username or password')
         }
 
         // 验证密码
-        const matched = await bcrypt.compare(password, retrievedUser.password);
+        const matched = await bcrypt.compare(password, retrievedUser.password)
         if (!matched) {
-            return res.status(401).send("Unauthorized: Invalid username or password");
+            return res
+                .status(401)
+                .send('Unauthorized: Invalid username or password')
         }
 
         // 生成JWT令牌
-        const token = await generateAccessToken(username);
-        res.status(200).send({ token });
-
+        const token = await generateAccessToken(username)
+        res.status(200).send({ token })
     } catch (error) {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error')
     }
 }
--- Application/backend-express/backend.js
+++ Application/backend-express/backend.js
@@ -1,64 +1,64 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import { registerUser, loginUser } from "./auth.js";
-dotenv.config();
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { registerUser, loginUser, conflictUser } from './auth.js'
+dotenv.config()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-const mongoUser = process.env.MONGO_USER;
-const mongoPwd = process.env.MONGO_PWD;
-const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoUser = process.env.MONGO_USER
+const mongoPwd = process.env.MONGO_PWD
+const mongoCluster = process.env.MONGO_CLUSTER
 
-let mongoURI;
+let mongoURI
 
 if (mongoUser && mongoPwd) {
-    mongoURI = `mongodb://${mongoUser}:${mongoPwd}@localhost:27017/${mongoCluster}?authSource=admin`;
+    //mongoURI = `mongodb://${mongoUser}:${mongoPwd}@localhost:27017/${mongoCluster}?authSource=admin`;
+    mongoURI = `mongodb+srv://${mongoUser}:${mongoPwd}@${mongoCluster}.yzdyxed.mongodb.net/StudyBuddy?retryWrites=true&w=majority&appName=StudyBuddy/`
 } else {
-    mongoURI = `mongodb://localhost:27017/${mongoCluster}`;
+    //mongoURI = `mongodb://localhost:27017/${mongoCluster}`;
 }
 
-mongoose.connect(mongoURI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
+mongoose
+    .connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+    })
 
-app.post("/signup", registerUser);
-app.post('/login', loginUser);
+app.post('/signup', registerUser)
+app.post('/login', loginUser)
 
 /* GET <server>/login/user?username=
                     <username>password=<password>
             returns user token */
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
     // generate the user token, do I import?
     // 200 Get request for successful retrieval
-    if (loginUser(req, res) === 200) {
-        console.log("Success retrieve")
-    }
-    else {console.log("Get did not work")};
+    return loginUser(req, res).then(() => {})
 })
 
 // POST <server>/user new user to db
-app.post("/users", (req, res) => {
-    const newUser = req.body;
-    const username = req.query.username;
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    const username = req.query.username
     // Check if username already exists
-    const existingUser = User.findOne({ username });
-        if (existingUser) {
-            return res.status(409).send("Username already taken");
+    conflictUser(username).then((conflict) => {
+        if (conflict) {
+            return res.status(409).send('Username already taken')
         }
         // Call register user if not exists
-        else return(registerUser(newUser, res));
+        else registerUser(req, res).then(() => {})
+    })
 })
 
 app.listen(port, () => {
-    console.log(
-        `app listening at http://localhost:${port}`
-    );
-});
+    console.log(`app listening at http://localhost:${port}`)
+})
