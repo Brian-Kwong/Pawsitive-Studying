5848bdf4a11133ce10a26a33688a208640aa5ebd
Auroraz25 <jz669696@gmail.com>
2024-05-27 01:24:24 +0800

    finish home page

--- Application/Pawsitive-Studying/app/(Main-App)/(Tabs)/index.jsx
+++ Application/Pawsitive-Studying/app/(Main-App)/(Tabs)/index.jsx
@@ -1,76 +1,144 @@
-import { StatusBar } from 'expo-status-bar'
-import { router } from 'expo-router'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Modal, TextInput, StyleSheet, FlatList } from 'react-native';
+import { fetchUserTasks , addUserTask } from './requests.js'
 
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Modal, TextInput, StyleSheet } from 'react-native';
-
-
-const sec_per_min = 60
-const gotoTimer = (time) => {
-    router.push({
-        pathname: `../Timer/timer_page`,
-        params: { time: time },
-    });
-};
 
 export default function Home() {
-    const [tasks, setTasks] = useState([
-        { id: 1, name: 'Task 1', time: '1 hour', category: 'Work' },
-        { id: 2, name: 'Task 2', time: '30 minutes', category: 'Study' },
-        { id: 3, name: 'Task 3', time: '45 minutes', category: 'Exercise' },
-    ]);
-
+    const [tasks, setTasks] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
-    const [newTask, setNewTask] = useState({ name: '', time: '', category: '' });
+    const [newTask, setNewTask] = useState({});
+    const [nameError, setNameError] = useState('');
+    const [timeError, setTimeError] = useState('');
+
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    async function fetchTasks() {
+        try {
+            const userTasks = await fetchUserTasks();
+            console.log("User tasks:", userTasks);
+            setTasks(userTasks.tasks)
+        } catch (error) {
+            console.error("Error fetching user tasks:", error);
+        }
+    }
+
+    async function addTask(newTask) {
+        try {
+            const addedTask = await addUserTask(newTask);
+            console.log("New task added:", addedTask);
+            // 更新任务列表
+            fetchTasks()
+        } catch (error) {
+            console.error("Error adding task:", error);
+        }
+    }
+
+    function closeNewTaskModal(){
+        setModalVisible(false);
+        setNewTask({});
+        setNameError('');
+        setTimeError('');
+    }
+
 
     const handleAddTask = () => {
-        setTasks([...tasks, { ...newTask, id: tasks.length + 1 }]);
-        setNewTask({ name: '', time: '', category: '' });
-        setModalVisible(false);
+        console.log(newTask)
+        if(!newTask.name){
+            setNameError('must full name')
+        }
+        if(!newTask.time){
+            setTimeError('must full time')
+        }
+
+        addTask(newTask);
+        closeNewTaskModal();
     };
+
+    const renderItem = ({ item }) => (
+        <View style={styles.task}>
+            <Text>{item.name}</Text>
+            <Text>{item.time}</Text>
+            <Text>{item.course}</Text>
+        </View>
+    );
 
 
     return (
         <View style={styles.container}>
             <Text style={styles.heading}>Tasks</Text>
-            {tasks.map(task => (
-                <View key={task.id} style={styles.task}>
-                    <Text>{task.name}</Text>
-                    <Text>{task.time}</Text>
-                    <Text>{task.category}</Text>
-                </View>
-            ))}
+            <FlatList
+                data={tasks}
+                renderItem={renderItem}
+                keyExtractor={item => item._id.toString()}
+            />
+
+
+
             <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
                 <Text style={styles.addButtonText}>Add Task</Text>
             </TouchableOpacity>
 
-            <Modal visible={modalVisible} animationType="slide">
-                <View style={styles.modalContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Task Name"
-                        value={newTask.name}
-                        onChangeText={text => setNewTask({ ...newTask, name: text })}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Time Needed"
-                        value={newTask.time}
-                        onChangeText={text => setNewTask({ ...newTask, time: text })}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Category"
-                        value={newTask.category}
-                        onChangeText={text => setNewTask({ ...newTask, category: text })}
-                    />
-                    <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
-                        <Text style={styles.addButtonText}>Add Task</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.cancelButton} onPress={() => setModalVisible(false)}>
-                        <Text style={styles.cancelButtonText}>Cancel</Text>
-                    </TouchableOpacity>
-                </View>
+            <Modal visible={modalVisible} animationType="fade" transparent={true}>
+                <TouchableOpacity style={styles.modalBackground}
+                    activeOpacity={1}
+                    onPress={() => closeNewTaskModal()} // 点击背景关闭模态
+                >
+                    <View style={styles.modalContainer} onStartShouldSetResponder={() => true}>
+                        <Text style={styles.modalHeading}>Add New Task</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Task Name"
+                            value={newTask.name}
+                            placeholderTextColor="#888"
+                            onChangeText={text => {
+                                setNewTask({ ...newTask, name: text });
+                                setNameError('');
+                            }}
+                        />
+                        {nameError ? <Text style={styles.error}>{nameError}</Text> : null}
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Time"
+                            placeholderTextColor="#888"
+                            value={newTask.time}
+                            onChangeText={text => {
+                                setNewTask({ ...newTask, time: text });
+                                setTimeError('');
+                            }}
+                        />
+                        {timeError ? <Text style={styles.error}>{timeError}</Text> : null}
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Course(optional)"
+                            placeholderTextColor="#888"
+                            value={newTask.category}
+                            onChangeText={text => setNewTask({ ...newTask, course: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Description(optional)"
+                            placeholderTextColor="#888"
+                            value={newTask.category}
+                            onChangeText={text => setNewTask({ ...newTask, description: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Points(optional)"
+                            placeholderTextColor="#888"
+                            value={newTask.category}
+                            onChangeText={text => setNewTask({ ...newTask, points: text })}
+                        />
+                        <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+                            <Text style={styles.addButtonText}>Add Task</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.cancelButton} onPress={() => closeNewTaskModal()}>
+                            <Text style={styles.cancelButtonText}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </TouchableOpacity>
             </Modal>
         </View>
     );
@@ -103,19 +171,33 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
     },
-    modalContainer: {
+    modalBackground: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 20,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContainer: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        width: '80%',
+    },
+    modalHeading: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
     input: {
         borderWidth: 1,
         borderColor: 'gray',
         borderRadius: 5,
         padding: 10,
-        marginBottom: 20,
-        width: '100%',
+        marginBottom: 10,
+    },
+    error: {
+        color: 'red',
+        marginBottom: 10,
     },
     cancelButton: {
         backgroundColor: 'red',
---
+++ Application/Pawsitive-Studying/app/(Main-App)/(Tabs)/requests.js
@@ -0,0 +1,68 @@
+import * as SecureStore from 'expo-secure-store';
+
+const baseURL = "https://studybuddyserver.azurewebsites.net/"; // URL for login requests
+
+
+function addAuthHeader(otherHeaders = {}){
+    const userToken = SecureStore.getItem('Token');
+    if(userToken != null){
+        return {
+            ...otherHeaders,
+            Authorization: `Bearer ${userToken}`
+        }
+    } else{
+        return otherHeaders;
+    }
+}
+
+
+export async function fetchUserTasks(){
+    try {
+        const user_id = await SecureStore.getItemAsync('user_id');
+
+        const url = baseURL + "users/" + user_id + "/tasks";
+
+        const response = await fetch(url, {
+            method:"get",
+            headers: addAuthHeader({
+                "Content-Type": "application/json"
+            })
+        })
+
+        if(response.ok) {
+            const data = await response.json();
+            return data;
+        }else{
+            throw new Error(`Failed to fetch user tasks: ${response.statusText}`);
+        }
+    }
+    catch (error) {
+        // 捕获任何可能的错误，并将其抛出
+        throw new Error(`Error fetching user tasks: ${error.message}`);
+    }
+}
+
+export async function addUserTask(newTask){
+    try {
+        const user_id = await SecureStore.getItemAsync('user_id');
+        const url = baseURL + "users/" + user_id + "/task";
+
+        const response = await fetch(url, {
+            method: "post",
+            headers: addAuthHeader({
+                "Content-Type": "application/json"
+            }),
+            body: JSON.stringify(newTask)
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error(`Failed to add user task: ${response.statusText}`);
+        }
+    } catch (error) {
+        // 捕获任何可能的错误，并将其抛出
+        throw new Error(`Error adding user task: ${error.message}`);
+    }
+}
