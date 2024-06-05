1d6c51ccfb4c334bdeea26bc52db58dde6082544
Brian Kwong <bkwong01@calpoly.edu>
2024-06-05 02:10:33 -0700

    fixed merge issue

--- Application/Pawsitive-Studying/app/(Main-App)/UserPages/requests.js
+++ Application/Pawsitive-Studying/app/(Main-App)/UserPages/requests.js
@@ -15,56 +15,55 @@ function addAuthHeader(otherHeaders = {}) {
     }
 }
 
-export async function searchSongs(query) {
+export async function fetchUserTasks() {
     try {
-        const headers = await addAuthHeader({
-            "Content-Type": "application/json",
-        });
+        const user_id = await SecureStore.getItemAsync("user_id");
 
-        const url = `${baseURL}searchSong?songName=${query}`;
+        const url = baseURL + "users/" + user_id + "/tasks";
+
         const response = await fetch(url, {
-            method: "GET",
-            headers: headers,
+            method: "get",
+            headers: addAuthHeader({
+                "Content-Type": "application/json",
+            }),
         });
 
         if (response.ok) {
             const data = await response.json();
             return data;
         } else {
-            console.error("Search API error:", response.status);
-            throw new Error(`Failed to search songs: ${response.statusText}`);
-        }
-    } catch (error) {
-        console.error("Error searching songs:", error);
-        throw new Error(`Error searching songs: ${error.message}`);
-    }
-}
-
-export async function addSongToPlaylist(playlistId, song) {
-    try {
-        const headers = await addAuthHeader({
-            "Content-Type": "application/json",
-        });
-        const userID = await getID();
-        const url = `${baseURL}users/${userID}/${playlistId}/song`;
-        const response = await fetch(url, {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(song),
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        } else {
-            console.error("Add song to playlist API error:", response.status);
             throw new Error(
-                `Failed to add song to playlist: ${response.statusText}`
+                `Failed to fetch user tasks: ${response.statusText}`
             );
         }
     } catch (error) {
-        console.error("Error adding song to playlist:", error);
-        throw new Error(`Error adding song to playlist: ${error.message}`);
+        // 捕获任何可能的错误，并将其抛出
+        throw new Error(`Error fetching user tasks: ${error.message}`);
+    }
+}
+
+export async function addUserTask(newTask) {
+    try {
+        const user_id = await SecureStore.getItemAsync("user_id");
+        const url = baseURL + "users/" + user_id + "/task";
+
+        const response = await fetch(url, {
+            method: "post",
+            headers: addAuthHeader({
+                "Content-Type": "application/json",
+            }),
+            body: JSON.stringify(newTask),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error(`Failed to add user task: ${response.statusText}`);
+        }
+    } catch (error) {
+        // 捕获任何可能的错误，并将其抛出
+        throw new Error(`Error adding user task: ${error.message}`);
     }
 }
 
@@ -138,5 +137,58 @@ export async function deleteUserTask(taskId) {
     } catch (error) {
         // 捕获任何可能的错误，并将其抛出
         throw new Error(`Error deleting task: ${error.message}`);
+    }
+}
+
+export async function searchSongs(query) {
+    try {
+        const headers = await addAuthHeader({
+            "Content-Type": "application/json",
+        });
+
+        const url = `${baseURL}searchSong?songName=${query}`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: headers,
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error("Search API error:", response.status);
+            throw new Error(`Failed to search songs: ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error("Error searching songs:", error);
+        throw new Error(`Error searching songs: ${error.message}`);
+    }
+}
+
+export async function addSongToPlaylist(playlistId, song) {
+    try {
+        const headers = await addAuthHeader({
+            "Content-Type": "application/json",
+        });
+        const userID = await getID();
+        const url = `${baseURL}users/${userID}/${playlistId}/song`;
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(song),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error("Add song to playlist API error:", response.status);
+            throw new Error(
+                `Failed to add song to playlist: ${response.statusText}`
+            );
+        }
+    } catch (error) {
+        console.error("Error adding song to playlist:", error);
+        throw new Error(`Error adding song to playlist: ${error.message}`);
     }
 }
